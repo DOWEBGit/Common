41f8a4a457d2f6bf612f478cc7be1c6e925c02eb
Davide <info@davidebertagna.it>
2024-02-26 18:38:52 +0100

    Notifiche PUSH

--- Include/Foot.php
+++ Include/Foot.php
@@ -13,7 +13,7 @@ if ($asyncArray && count($asyncArray) > 0)
 
         foreach ($asyncArray as $id)
         {
-            ?>
+        ?>
 
         ReloadView(<?=$id?>);
 
@@ -31,45 +31,41 @@ if (\Common\Client::$enabled)
 {
     ?>
 
-    <script src="https://static.doweb.site/liveserver/jquery_slim.js"></script>-->
-    <script src="https://static.doweb.site/liveserver/signalR.js"></script>
+    <script src="https://static.doweb.site/liveserver/signalRJs.js"></script>
 
     <script type="text/javascript">
 
-        $(function () {
+        var connection = hubConnection("/client", {useDefaultPath: false});
 
-            var connection = $.hubConnection("/client", { useDefaultPath: false });
-
-            connection.connectionSlow(function () {
-                console.log('DOWEB Client: We are currently experiencing difficulties with the connection.')
-            });
-
-            //connection.start({ transport: ['webSockets', 'longPolling'] });
-
-            connection.url = "<?= \Common\SiteVars::Value(\Common\VarsEnum::webpath) ?>/client";
-
-            var proxy = connection.createHubProxy('Client');
-
-            proxy.on('Push', function (name, value)
-            {
-                if (typeof Push === 'function') {
-                    Push(name, value);
-                }
-            });
-
-            //connection.logging = true;
-            connection.start({ pingInterval: 10000 })
-                .done(function ()
-                {
-                    console.log("DOWEB Client connected");
-                })
-                .fail(function (e)
-                {
-                    console.log("DOWEB Client failed: " + e);
-                });
-
+        connection.connectionSlow(function ()
+        {
+            console.log('DOWEB Client: We are currently experiencing difficulties with the connection.')
         });
 
+        //connection.start({ transport: ['webSockets', 'longPolling'] });
+
+        connection.url = "<?= \Common\SiteVars::Value(\Common\VarsEnum::webpath) ?>/client";
+
+        var proxy = connection.createHubProxy('Client');
+
+        proxy.on('Push', function (name, value)
+        {
+            if (typeof Push === 'function')
+            {
+                Push(name, value);
+            }
+        });
+
+        //connection.logging = true;
+        connection.start({pingInterval: 10000})
+            .done(function ()
+            {
+                console.log("DOWEB Client connected");
+            })
+            .fail(function (e)
+            {
+                console.log("DOWEB Client failed: " + e);
+            });
 
     </script>
 
