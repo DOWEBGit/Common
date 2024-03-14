abaa05457904fad357a1ff1cf5c5622f37e299e4
Davide <info@davidebertagna.it>
2024-03-14 11:43:06 +0100

    Fix su array bodytostate

--- State.php
+++ State.php
@@ -14,60 +14,60 @@ class State
     //TempState non mantiene tra chiamate, solo una volta, uso due array,
     //uno per leggere e l'altro per scrivere in modo da perdere il contenuto vecchio e mantenere quello nuovo
     public static function TempWrite(string $name, string $value) : void
-    {   
+    {
         //scrivo sia sul Read che sul Write in modo che se leggo quello che scrivo nello stesso codice il risultato è uniforme
         //ad ogni modo nell'output mi porto dietro solo il _TempStateWrite
-        
+
         $name = strtolower($name);
-        
-        $windowState = array();
-        
+
+        $windowState = [];
+
         if (isset($GLOBALS['_TempStateWrite']))
         {
             $json = $GLOBALS['_TempStateWrite'];
-            
+
             $windowState = json_decode(base64_decode($json), true);
 
             if ($windowState === null)
-                $windowState = array();
-        }        
-        
+                $windowState = [];
+        }
+
         $windowState[$name] = strval($value);
-        
+
         $newJson = base64_encode(json_encode($windowState));
-        
+
         $GLOBALS['_TempStateWrite'] = $newJson;
-        
-        
-        
-        $windowState = array();
+
+
+
+        $windowState = [];
 
         if (isset($GLOBALS['_TempStateRead']))
         {
             $json = $GLOBALS['_TempStateRead'];
-            
+
             $windowState = json_decode(base64_decode($json), true);
 
             if ($windowState === null)
-                $windowState = array();
-        }        
-        
+                $windowState = [];
+        }
+
         $windowState[$name] = strval($value);
-        
+
         $newJson = base64_encode(json_encode($windowState));
-        
+
         $GLOBALS['_TempStateRead'] = $newJson;
     }
-    
+
     public static function TempRead(string $name) : string
-    {            
+    {
         $name = strtolower($name);
-        
+
         if (isset($GLOBALS['_TempStateRead']))
         {
             $json = $GLOBALS['_TempStateRead'];
 
-            $windowState = array();
+            $windowState = [];
 
             if ($json)
             {
@@ -80,46 +80,46 @@ class State
             if (isset($windowState[$name]))
                 return $windowState[$name];
         }
-        
+
         return "";
     }
-    
+
 
     public static function WindowWrite(string $name, string $value) : void
-    {            
+    {
         $name = strtolower($name);
-        
-        $windowState = array();
-        
+
+        $windowState = [];
+
         if (isset($GLOBALS['_WindowState']))
         {
             $json = $GLOBALS['_WindowState'];
-            
+
             $windowState = json_decode(base64_decode($json), true);
 
             if ($windowState === null)
-                $windowState = array();
+                $windowState = [];
         }
-        
+
         if (isset($windowState[$name]) && $windowState[$name] === $value)
             return;
-        
+
         $windowState[$name] = strval($value);
-        
+
         $newJson = base64_encode(json_encode($windowState));
-        
+
         $GLOBALS['_WindowState'] = $newJson;
     }
-    
+
     public static function WindowRead(string $name, string $default = "") : string
-    {            
+    {
         $name = strtolower($name);
-        
+
         if (isset($GLOBALS['_WindowState']))
         {
             $json = $GLOBALS['_WindowState'];
 
-            $windowState = array();
+            $windowState = [];
 
             if ($json)
             {
@@ -132,7 +132,7 @@ class State
             if (isset($windowState[$name]) && $windowState[$name] != "")
                 return $windowState[$name];
         }
-        
+
         return $default;
     }
 
@@ -145,72 +145,75 @@ class State
     }
 
     public static function SessionWrite(string $name, string $value) : void
-    {            
-        if (session_status() == PHP_SESSION_NONE)
-            session_start();            
-        
-        $_SESSION[strtolower($name)] = $value;
-    }
-    
-    public static function SessionRead(string $name) : string
-    {            
+    {
         if (session_status() == PHP_SESSION_NONE)
             session_start();
-        
+
+        $_SESSION[strtolower($name)] = $value;
+    }
+
+    public static function SessionRead(string $name) : string
+    {
+        if (session_status() == PHP_SESSION_NONE)
+            session_start();
+
         $name = strtolower($name);
-        
+
         if (!isset($_SESSION[$name]))
             return "";
-        
+
         return $_SESSION[$name];
-    }   
+    }
 
 
     public static function WriteToHtml() : void
-    {       
+    {
         if (isset($GLOBALS['_WindowState']))
             echo '<input type="hidden" id="WindowState" value="' . $GLOBALS['_WindowState'] . '">';
         else
-            echo '<input type="hidden" id="WindowState" value="">';                    
+            echo '<input type="hidden" id="WindowState" value="">';
 
-        
+
         if (isset($GLOBALS['_TempStateWrite']))
             echo '<input type="hidden" id="TempState" value="' . $GLOBALS['_TempStateWrite'] . '">';
         else
-            echo '<input type="hidden" id="TempState" value="">';                            
+            echo '<input type="hidden" id="TempState" value="">';
     }
-    
+
     //sessionstate lo scrivo tramite javascript direttamente con una api SetSessionState
-    //chiamato da baseview e da baseaction    
-    
+    //chiamato da baseview e da baseaction
+
     public static function BodyToState() : void
     {
         if (!isset($_POST))
             return;
-        
+
         $json = $_POST["INPUTSTREAM"];
-        
+
         $stateArray = json_decode($json, true);
-        
-        if ($stateArray === null || !is_array($stateArray) || count($stateArray) != 2)
-            return;                    
-                
-        $GLOBALS['_TempStateRead'] = $stateArray[0];        
-        $GLOBALS['_WindowState'] = $stateArray[1];        
+
+        if (!is_array($stateArray) || count($stateArray) != 2)
+            return;
+
+        if ($stateArray[0] == null || $stateArray[1] == null)
+            return;
+
+        $GLOBALS['_TempStateRead'] = $stateArray[0];
+        $GLOBALS['_WindowState'] = $stateArray[1];
     }
-    
+
     public static function StateToBody() : void
-    {        
-        if (!isset($GLOBALS['_TempStateWrite']))            
-            $resultArray[] = "";           
+    {
+        if (!isset($GLOBALS['_TempStateWrite']))
+            $resultArray[] = "";
         else
-            $resultArray[] = $GLOBALS['_TempStateWrite'];           
-        
+            $resultArray[] = $GLOBALS['_TempStateWrite'];
+
         if (!isset($GLOBALS['_WindowState']))
-            $resultArray[] = "";           
+            $resultArray[] = "";
         else
-            $resultArray[] = $GLOBALS['_WindowState'];        
-        
+            $resultArray[] = $GLOBALS['_WindowState'];
+
         echo json_encode($resultArray);
     }
 }
