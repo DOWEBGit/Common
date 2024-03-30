579427e2cecc7963c87f06403bc30fbfb1b2ba9b
Davide <info@davidebertagna.it>
2024-03-30 11:18:58 +0100

    \Common\View\Server::View(); automatica, senza necessità del nome

--- View/Server.php
+++ View/Server.php
@@ -5,8 +5,27 @@ namespace Common\View;
 
 class Server
 {
-    public static function View(string $viewName) : void
+    public static function View(string $viewName = ""): void
     {
+        if ($viewName == "")
+        {
+            $url = $_GET["url"];
+
+            // Verifica se $url inizia con "/xx/" dove xx sono due caratteri qualsiasi
+            if (preg_match('/^\/.{2}\//', $url))
+            {
+                // Rimuovi i primi due caratteri
+                $url = substr($url, 3);
+            }
+
+            $viewName = $url;
+
+            $viewName = str_replace("/", "\\", $viewName);
+
+            if (str_starts_with($viewName, "\\"))
+                $viewName = substr($viewName, 1);
+        }
+
         $className = "\\View\\" . $viewName;
 
         // Verifica se il metodo corrispondente all'azione esiste nella classe corrente
