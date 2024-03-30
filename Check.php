da4a58c74e83368c55ddb3318b768e81a72d439d
Davide <info@davidebertagna.it>
2024-03-30 12:01:02 +0100

    Tolta roba inutile

--- Check.php
+++ Check.php
@@ -1,11 +1,6 @@
 <?php
 declare(strict_types=1);
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
- */
-
 namespace Common;
 
 /**
