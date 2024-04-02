0e315ecd1c51918cd2ec26722ceeabc304110a74
Andrea <melasio@hotmail.com>
2024-04-02 11:50:47 +0200

    aggiunto CodeGenerato.php

---
+++ CodeGenerator/CodeGenerator.php
@@ -0,0 +1,10 @@
+<?php
+include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/AreeControlliEnum.php');
+include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/Controller.php');
+include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/EtichetteEnum.php');
+include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/GetLink.php');
+include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/Model.php');
+include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/PagineControlliEnum.php');
+include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/PagineDatiControlliEnum.php');
+include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/PagineDatiEnum.php');
+include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/PagineEnum.php');
