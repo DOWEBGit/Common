7d0e22aa103ab18ae89bd79f1dc4cc403a7ba440
Andrea <melasio@hotmail.com>
2024-04-04 09:37:19 +0200

    Modificato l'ordine degli include perchè Model.php sparava degli header e mi riempiva il log error di Warning: Cannot modify header information - headers already sent

--- CodeGenerator/CodeGenerator.php
+++ CodeGenerator/CodeGenerator.php
@@ -3,8 +3,8 @@ include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/Aree
 include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/Controller.php');
 include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/EtichetteEnum.php');
 include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/GetLink.php');
-include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/Model.php');
 include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/PagineControlliEnum.php');
 include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/PagineDatiControlliEnum.php');
 include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/PagineDatiEnum.php');
 include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/PagineEnum.php');
+include_once ($_SERVER["DOCUMENT_ROOT"] . '/public/php/Common/CodeGenerator/Model.php');
