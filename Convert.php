51266f7f0083d3ee9df30e5f1adc1368e8c6e2c8
Andrea <melasio@hotmail.com>
2024-06-12 15:32:14 +0200

    Aggiunta FilterFilename per avere un nome file valido a partire da una stringa

--- Convert.php
+++ Convert.php
@@ -307,4 +307,54 @@ class Convert
 
         return $link . $querystring;
     }
+
+    private static function BeautifyFilename(string $filename) : string
+    {
+        // reduce consecutive characters
+        $filename = preg_replace(array(
+             // "file   name.zip" becomes "file-name.zip"
+             '/ +/',
+             // "file___name.zip" becomes "file-name.zip"
+             '/_+/',
+             // "file---name.zip" becomes "file-name.zip"
+             '/-+/'
+         ), '-', $filename);
+        $filename = preg_replace(array(
+             // "file--.--.-.--name.zip" becomes "file.name.zip"
+             '/-*\.-*/',
+             // "file...name..zip" becomes "file.name.zip"
+             '/\.{2,}/'
+         ), '.', $filename);
+        // lowercase for windows/unix interoperability http://support.microsoft.com/kb/100625
+        $filename = mb_strtolower($filename, mb_detect_encoding($filename));
+        // ".file-name.-" becomes "file-name"
+        $filename = trim($filename, '.-');
+        return $filename;
+    }
+
+    /**
+     * @param string $filename <p>Il filename da fixare</p>
+     * @param bool $beautify
+     * @return string
+     */
+    public static function FilterFilename(string $filename, bool $beautify = true) {
+        // sanitize filename
+        $filename = preg_replace(
+            '~
+        [<>:"/\\|?*]|            # file system reserved https://en.wikipedia.org/wiki/Filename#Reserved_characters_and_words
+        [\x00-\x1F]|             # control characters http://msdn.microsoft.com/en-us/library/windows/desktop/aa365247%28v=vs.85%29.aspx
+        [\x7F\xA0\xAD]|          # non-printing characters DEL, NO-BREAK SPACE, SOFT HYPHEN
+        [#\[\]@!$&\'()+,;=]|     # URI reserved https://tools.ietf.org/html/rfc3986#section-2.2
+        [{}^\~`]                 # URL unsafe characters https://www.ietf.org/rfc/rfc1738.txt
+        ~x',
+            '-', $filename);
+        // avoids ".", ".." or ".hiddenFiles"
+        $filename = ltrim($filename, '.-');
+        // optional beautification
+        if ($beautify) $filename = self::BeautifyFilename($filename);
+        // maximize filename length to 255 bytes http://serverfault.com/a/9548/44086
+        $ext = pathinfo($filename, PATHINFO_EXTENSION);
+        $filename = mb_strcut(pathinfo($filename, PATHINFO_FILENAME), 0, 255 - ($ext ? strlen($ext) + 1 : 0), mb_detect_encoding($filename)) . ($ext ? '.' . $ext : '');
+        return $filename;
+    }
 }
