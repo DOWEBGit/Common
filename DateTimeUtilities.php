3c5e2c892abb829c3b5be74f8041708b068d388a
Andrea <melasio@hotmail.com>
2024-09-30 10:23:17 +0200

    aggiunta la classe DateTimeUtilities.php, che mi serviva una funzione per ritornare la data di inizio e fine del numero della settimana (tipo la 30isma settimana del 2022)

---
+++ DateTimeUtilities.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Common;
+
+class DateTimeUtilities
+{
+    /**<p>Ritorna la data di inizio e la data fine del numero settimana relativo all'anno passati come parametri</p>
+     * <p>Entrambe le date sono classi DateTime e possono essere lette usando $result->DataInizio e $result->DataFine</p>
+     * @param $numeroSettimana
+     * @param $anno
+     * @return array
+     */
+    public static function GetStartAndEndDate(int $numeroSettimana, int $anno): \stdClass
+    {
+        $data = new \DateTime('midnight');
+        //clono se no ->modify mi modifica anche la data di inizio
+        $result = new \stdClass();
+        $result->DataInizio = clone $data->setISODate($numeroSettimana, $anno);
+        $result->DataFine = $data->modify('+6 days');
+        return $result;
+    }
+}
